d5c6728c3e958526e16f29bdc749e48a30a0ddc1
dawid <dpilif@gmail.com>
2021-08-08 17:57:43 +0200

    Url comments added and removed unneeded import.

--- src/main/java/com/pl/df/api/UserController.java
+++ src/main/java/com/pl/df/api/UserController.java
@@ -1,7 +1,6 @@
 package com.pl.df.api;
 
 import java.net.URI;
-import java.util.Collection;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
@@ -19,7 +18,6 @@ import com.pl.df.model.User;
 import com.pl.df.repository.RoleRepo;
 import com.pl.df.service.UserService;
 
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -36,29 +34,33 @@ public class UserController {
 		return ResponseEntity.ok().body(userService.getUsers());
 	}
 	
+	// http://localhost:8088/api/users/{id}
 	@GetMapping("/users/{id}")
 	public ResponseEntity<User> getUserById(@PathVariable("id") Long id) {	// additional method
 		return ResponseEntity.ok().body(userService.getUserById(id));
 	}
-	@GetMapping("/role/{id}")
+	// http://localhost:8088/api/roles/{id}
+	@GetMapping("/roles/{id}")
 	public ResponseEntity<Role> getRoleById(@PathVariable("id") Long id) {	// additional method
 		return ResponseEntity.ok().body(roleRepo.getById(id));
 	}
 
 
-	
+	// http://localhost:8088/api/users
 	@PostMapping("/users")
 	public ResponseEntity<User> saveUser(@RequestBody User user) {
 		User savedUser = userService.saveUser(user);
 		return ResponseEntity.created(createURI("/api/users/" + savedUser.getId())).body(savedUser);
 	}
 	
+	// http://localhost:8088/api/role
 	@PostMapping("/role")
 	public ResponseEntity<Role> saveRole(@RequestBody Role role) {
 		Role savedRole = userService.saveRole(role);
 		return ResponseEntity.created(createURI("/api/role/" + savedRole.getId())).body(savedRole);
 	}
 	
+	// http://localhost:8088/api/add-to-user
 	@PostMapping("/role/add-to-user")
 	public ResponseEntity<Void> addRoleToUser(@RequestBody RoleToUserForm roleToUserForm) {
 		userService.addRoleToUser(roleToUserForm.getUsername(), roleToUserForm.getRolename());
