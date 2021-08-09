2805fa2002fb76f95a7a4359d650b31a603d3bc2
dawid <dpilif@gmail.com>
2021-08-09 21:43:21 +0200

    Encoding password before saving User into database implementation added.

--- src/main/java/com/pl/df/service/UserServiceImpl.java
+++ src/main/java/com/pl/df/service/UserServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.pl.df.model.Role;
@@ -28,6 +29,7 @@ public class UserServiceImpl implements UserService, UserDetailsService {
 
 	private final UserRepo userRepo;
 	private final RoleRepo roleRepo;
+	private final PasswordEncoder passwordEncoder;
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -56,6 +58,9 @@ public class UserServiceImpl implements UserService, UserDetailsService {
 	@Override
 	public User saveUser(User user) {
 		log.info("saveUser: " + user.getUsername());
+
+		user.setPassword(passwordEncoder.encode(user.getPassword())); // encoding password before saving into database 
+		
 		return userRepo.save(user);
 	}
 
