d4489ca10e562a55675b2a3787ef335b6ac4254b
dawid <dpilif@gmail.com>
2021-08-28 18:25:54 +0200

    Removed unused imports from User controller.

--- src/main/java/com/pl/df/api/UserController.java
+++ src/main/java/com/pl/df/api/UserController.java
@@ -1,38 +1,19 @@
 package com.pl.df.api;
 
 import static com.pl.df.configuration.AppRole.USER;
-import static com.pl.df.configuration.JwtUtility.*;
 
-import java.io.IOException;
-import java.net.URI;
 import java.util.ArrayList;
-import java.util.Base64;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.auth0.jwt.interfaces.DecodedJWT;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.pl.df.dto.RoleToUserForm;
 import com.pl.df.dto.UserForm;
 import com.pl.df.model.Role;
 import com.pl.df.model.User;
